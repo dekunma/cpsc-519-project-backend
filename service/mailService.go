ecd4e478559e9fecf0dda8e3f369452004989708
Dekun Ma <william@dekun.me>
2024-02-18 14:31:46 -0500

    Add mailer name when sending email

--- service/mailService.go
+++ service/mailService.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"github.com/go-mail/mail"
 	"os"
+	"strconv"
+	"time"
 )
 
 func SendVerificationCode(code, toEmail string) error {
 	m := mail.NewMessage()
-	m.SetHeader("From", os.Getenv("SMTP_USERNAME"))
+	m.SetHeader("From", "NAME TBD Admin"+"<"+os.Getenv("SMTP_USERNAME")+">")
 	m.SetHeader("To", toEmail)
 	m.SetHeader("Subject", "Verification Code for NAME TBD")
+	messageId := "<" + strconv.FormatInt(time.Now().Unix(), 10) + "@dekun.me" + ">"
+	m.SetHeader("Message-Id", messageId)
 	m.SetBody("text/html", "Your verification code is: <br/> <b>"+code+"</b> <br/> <br/> This code will expire in 10 minutes.")
 	d := mail.NewDialer(os.Getenv("SMTP_HOST"), 465, os.Getenv("SMTP_USERNAME"), os.Getenv("SMTP_PASSWORD"))
 	if err := d.DialAndSend(m); err != nil {
